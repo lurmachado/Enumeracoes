d77baac29471d831f570f84b2fb27bb47bde8c6b
Luciano Machado <lurmachado@hotmail.com>
2024-03-26 20:18:19 -0300

    Enumerações

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+namespace Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order1 = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+
+            Console.WriteLine(order1);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+
+
+        }
+    }
+}
